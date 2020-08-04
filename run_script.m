98af6f55a8a094475fdaa9ccb0b7c0738f0eb1f6
root <root@ip-172-31-27-28.ec2.internal>
2020-08-04 20:45:50 +0000

    error

--- run_script.m
+++ run_script.m
@@ -1,12 +1,12 @@
-% c4_set_parameters
-% c4_build_catalog
-% c4_preload_qsos
-% cd minFunc_2012
-% addpath(genpath(pwd))
-% mexAll
-% train_release = 'dr7';
-% cd ..
-% catalog = load(sprintf('%s/catalog', processed_directory(training_release)));
+c4_set_parameters
+c4_build_catalog
+c4_preload_qsos
+cd minFunc_2012
+addpath(genpath(pwd))
+mexAll
+cd ..
+training_release = 'dr7';
+catalog = load(sprintf('%s/catalog', processed_directory(training_release)));
 train_ind_all= [' ~ismember(all_QSO_ID, c4_QSO_ID) & ' '(catalog.filter_flags==0)'];
 tif = eval(train_ind_all);
 train_ind = randsample(tif, int32(0.5*numel(tif)));
